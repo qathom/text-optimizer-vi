6118cbf697aabe3bdd7752aefd31f54dcf52085a
Anthony Meizoso <ameizoso@logitech.com>
2020-11-09 19:50:30 +0100

    Added event listener

--- src/components/TimelineChart.tsx
+++ src/components/TimelineChart.tsx
@@ -3,9 +3,10 @@ import { Line } from 'react-chartjs-2';
 
 type Props = {
   data: number[];
+  onLabelClicked: (labelIndex: number) => void,
 };
 
-const TimelineChart: FunctionComponent<Props> = ({ data }) => {
+const TimelineChart: FunctionComponent<Props> = ({ data, onLabelClicked }) => {
   const chartData = {
     datasets: [{
       label: 'Sentiment per sentence',
@@ -16,11 +17,22 @@ const TimelineChart: FunctionComponent<Props> = ({ data }) => {
     labels: data.map((s, i) => `${i + 1}`),
   };
 
+  const onElementClick = (elements) => {
+    if (elements.length === 0) {
+      return;
+    }
+
+    const [first] = elements;
+
+    onLabelClicked(first._index);
+  };
+
   return (
     <div style={{ height: '200px' }}>
       <Line
         data={chartData}
         height={200}
+        onElementsClick={onElementClick}
         options={{
           maintainAspectRatio: false,
           responsive: true,
@@ -28,8 +40,8 @@ const TimelineChart: FunctionComponent<Props> = ({ data }) => {
             yAxes: [{
               display: true,
               ticks: {
-                min: -1,
-                max: 1,
+                // min: -1,
+                // max: 1,
               },
               scaleLabel: {
                 display: true,
