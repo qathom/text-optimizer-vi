de1a794cd9bbcae0d971687d73d30477c6f442ef
qathom <anthony.me@protonmail.ch>
2020-12-01 23:12:08 +0100

    Added imports

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,8 @@ import React from 'react';
 import GridContainer from './containers/GridContainer';
 import DefaultLayout from './layouts/Default';
 
+import 'intro.js/introjs.css';
+import 'intro.js/themes/introjs-dark.css';
 import './App.scss';
 
 function App() {
