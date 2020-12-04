1c311051ca7fd07fa86728be05739b9e8eb95950
qathom <anthony.me@protonmail.ch>
2020-12-04 22:41:21 +0100

    Fixed rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,12 +1,14 @@
 module.exports = {
   env: {
     browser: true,
-    es2021: true,
   },
   extends: [
-    'react-app',
+    'eslint:recommended',
+    'prettier/@typescript-eslint',
     'plugin:react/recommended',
     'airbnb',
+    'prettier',
+    'prettier/react',
   ],
   parser: '@typescript-eslint/parser',
   parserOptions: {
@@ -22,5 +24,10 @@ module.exports = {
   ],
   rules: {
     'react/jsx-filename-extension': [2, { extensions: ['.js', '.jsx', '.ts', '.tsx'] }],
+    'quotes': [2, 'single', { 'avoidEscape': true }],
+    'no-unused-vars': 'off',
+    '@typescript-eslint/no-unused-vars': 'error',
+    'no-use-before-define': 'off',
+    '@typescript-eslint/no-use-before-define': 'off',
   },
 };
