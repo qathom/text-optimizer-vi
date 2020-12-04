b51ad8dec296648a66f4a62fb29130e886ac0a1a
Anthony Meizoso <ameizoso@logitech.com>
2020-12-04 17:56:08 +0100

    Updated release script

--- bin/release.ts
+++ bin/release.ts
@@ -12,23 +12,15 @@ async function execute(command: string) {
 }
 
 (async () => {
-  // Get current branch
-  const branch = await execute('git rev-parse --abbrev-ref HEAD');
-
   try {
     const semVers = ['patch', 'minor', 'major'];
+
     const { semVer } = await inquirer.prompt({
       type: 'list',
       name: 'semVer',
       message: 'Semver:',
       choices: semVers,
     });
-
-    // Checkout
-    await execute('git checkout master');
-
-    // Merge
-    await execute('git merge dev --no-ff');
 
     // Update the version in package.json, create tag
     await execute(`npm version ${semVer} -m "Upgrade to %s"`);
@@ -39,8 +31,5 @@ async function execute(command: string) {
     log('Success!');
   } catch (e) {
     log('An error occurred', e);
-  } finally {
-  // Switck back to active branch
-    await execute(`git checkout ${branch}`);
   }
 })();
