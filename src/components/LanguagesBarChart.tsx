9c083fd3b84f896f3b636c408f0fd62820258b84
Anthony Meizoso <ameizoso@logitech.com>
2020-12-04 18:36:12 +0100

    Fixed stacked bar chart

--- src/components/LanguagesBarChart.tsx
+++ src/components/LanguagesBarChart.tsx
@@ -1,73 +1,55 @@
-import React, { FunctionComponent } from 'react';
-import { Bar } from 'react-chartjs-2';
+import React, { FunctionComponent, useEffect, useState } from 'react';
+import chartjs, { Chart } from 'chart.js';
+import { Bar, ChartData } from 'react-chartjs-2';
 
 type Props = {
   data: Map<string, number[]>;
-  //onLabelClicked: (labelIndex: number) => void,
 };
+
+const colors = [
+  '#1abc9c',
+  '#3498db',
+  '#9b59b6',
+  '#e67e22',
+  '#e74c3c',
+];
+
+function getChartData(data: Map<string, number[]>) {
+  // 1 dataset per language
+  const keys = [...data.keys()];
+  const datasets: Chart.ChartDataSets[] = [];
+
+  keys.forEach((lang, i) => {
+    datasets.push({
+      label: lang,
+      backgroundColor: colors[i],
+      borderColor: colors[i],
+      borderWidth: 1,
+      stack: `${lang}-${i}`,
+      data: data.get(lang),
+    });
+  });
+
+  return {
+    datasets,
+    labels: [...data.values()].map((_value, i) => `Paragraphe ${i}`),
+  };
+}
 
 const LanguagesBarChart: FunctionComponent<Props> = ({ data }) => {
-  const chartData = {
-    labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
-    datasets: [
-    //   {
-    //     label: 'My First dataset',
-    //     backgroundColor: 'rgba(255,99,132,0.2)',
-    //     borderColor: 'rgba(255,99,132,1)',
-    //     borderWidth: 1,
-    //     stack: 1,
-    //     hoverBackgroundColor: 'rgba(255,99,132,0.4)',
-    //     hoverBorderColor: 'rgba(255,99,132,1)',
-    //     data: [65, 59, 80, 81, 56, 55, 40]
-    //   },
-    //   {
-    //     label: 'My second dataset',
-    //     backgroundColor: 'rgba(155,231,91,0.2)',
-    //     borderColor: 'rgba(255,99,132,1)',
-    //     borderWidth: 1,
-    //     stack: 1,
-    //     hoverBackgroundColor: 'rgba(255,99,132,0.4)',
-    //     hoverBorderColor: 'rgba(255,99,132,1)',
-    //     data: [45, 79, 10, 41, 16, 85, 20]
-    //   }
-    Array.from(data).map(([lang, tabNumber], idx) =>{
-      {
-         label: 'My First dataset',
-         backgroundColor: 'rgba(255,99,132,0.2)',
-         borderColor: 'rgba(255,99,132,1)',
-         borderWidth: 1,
-         stack: 1,
-         hoverBackgroundColor: 'rgba(255,99,132,0.4)',
-         hoverBorderColor: 'rgba(255,99,132,1)',
-         data: [65, 59, 80, 81, 56, 55, 40]
-       },
-    })
-    ]
-};
-
-  const chartData = 
-
-  const onElementClick = (elements) => {
-    if (elements.length === 0) {
-      return;
-    }
-
-    const [first] = elements;
-
-    //onLabelClicked(first._index);
-  };
+  const chartData = getChartData(data);
 
   return (
     <div className="timeline-chart-container" style={{ height: '200px' }}>
       <Bar
         data={chartData}
         height={200}
-        onElementsClick={onElementClick}
         options={{
           maintainAspectRatio: false,
           responsive: true,
           scales: {
             yAxes: [{
+              stacked: true,
               display: true,
               ticks: {
                 // min: -1,
@@ -79,6 +61,7 @@ const LanguagesBarChart: FunctionComponent<Props> = ({ data }) => {
               },
             }],
             xAxes: [{
+              stacked: true,
               ticks: {
                 padding: 20,
               },
