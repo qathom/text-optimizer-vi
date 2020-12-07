d687afb0d924483526e4073a0016e171614b7162
qathom <anthony.me@protonmail.ch>
2020-12-07 20:24:58 +0100

    Added intro objects

---
+++ src/utils/intro.ts
@@ -0,0 +1,42 @@
+export default [
+  {
+    element: '.timeline-chart-container',
+    intro: 'The sentiment score is negative when it is below 0 and positive when the score is above 0. The value 0 represents a perfect neutrality score.',
+    position: 'top',
+  },
+  {
+    element: '.timeline-chart-container',
+    intro: 'Click on a square point to focus the item in the editor.',
+    position: 'left',
+  },
+  {
+    element: '.timeline-chart-container',
+    intro: 'Scroll in the chart to zoom in or zoom out.',
+    position: 'top',
+  },
+  {
+    element: '.neutrality-score',
+    intro: 'The overall neutrality score is displayed here.',
+    position: 'top',
+  },
+  {
+    element: '.neutrality-variance',
+    intro: 'The variance measures how far a set of numbers is spread out from their average value.',
+    position: 'top',
+  },
+  {
+    element: '[data-rb-event-key="Languages"]',
+    intro: 'Let\'s move to the language tab',
+    position: 'top',
+  },
+  {
+    element: '.languages-tab',
+    intro: 'Here we detect the languages. You can identify words used in different languages.',
+    position: 'top',
+  },
+  {
+    element: '.custom-switch',
+    intro: 'Finally, you might want to enable coloring for colorblindness',
+    position: 'top',
+  },
+];
