3a5b011c29ec82ad0c2c75c3bcaee24191b020a0
qathom <qathom@users.noreply.github.com>
2020-12-10 23:12:59 +0000

    deploy: daf73a64d1dbd499fd214382c6c0fb41a1ff6192

---
+++ precache-manifest.95b37c522827e7cc314e9dcdcf93c82d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "18505a32014a7520254e2c11fe568513",
+    "url": "/text-optimizer-vi/index.html"
+  },
+  {
+    "revision": "22e101a49d34c74d5fed",
+    "url": "/text-optimizer-vi/static/css/2.29237bf3.chunk.css"
+  },
+  {
+    "revision": "715db46ea51ad370f3e7",
+    "url": "/text-optimizer-vi/static/css/main.0f56bae9.chunk.css"
+  },
+  {
+    "revision": "22e101a49d34c74d5fed",
+    "url": "/text-optimizer-vi/static/js/2.872a3904.chunk.js"
+  },
+  {
+    "revision": "586ed6d6f9e261339c839a15d291879f",
+    "url": "/text-optimizer-vi/static/js/2.872a3904.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "715db46ea51ad370f3e7",
+    "url": "/text-optimizer-vi/static/js/main.8aa866e7.chunk.js"
+  },
+  {
+    "revision": "5c5b6726c0029b401ab5",
+    "url": "/text-optimizer-vi/static/js/runtime-main.d40a95cb.js"
+  }
+]);
